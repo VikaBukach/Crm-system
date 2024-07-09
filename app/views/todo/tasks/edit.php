d1ae8949abdb435725628d815cef965e572807e7
vikabukach <bukachviko@gmail.com>
2024-07-09 10:42:03 +0300

    style tag

--- app/views/todo/tasks/edit.php
+++ app/views/todo/tasks/edit.php
@@ -96,6 +96,58 @@ ob_start();
         </div>
     </div>
 </form>
+<script>
+    const tagInput = document.querySelector('#tag-input');
+    const tagsContainer = document.querySelector('.tags-container');
+    const hiddenTags = document.querySelector('#hidden-tags');
+    const existingTags = '<?= htmlspecialchars(isset($task['tags']) ?$task['tags'] : '') ?>';
+
+    function createTag(text){
+        const tag = document.createElement('div');
+        tag.classList.add('tag');
+        const tagText = document.createElement('span');
+        tagText.textContent = text;
+
+        const closeButton = document.createElement('button');
+        closeButton.innerHTML = '&times';
+
+        closeButton.addEventListener('click', () => {
+            tagsContainer.removeChild(tag);
+            updateHiddenTags();
+        });
+
+        tag.appendChild(tagText);
+        tag.appendChild(closeButton);
+
+        return tag;
+    }
+
+    function updateHiddenTags() {
+        const tags = tagsContainer.querySelectorAll('.tag span');
+        const tagText = Array.from(tags).map(tag => tag.textContent);
+        hiddenTags.value = tagText.join(',');
+    }
+
+    tagInput.addEventListener('input', (e) => {
+        if(e.target.value.includes(',')) {
+            const tagText = e.target.value.slice(0, -1).trim();
+            if (tagText.length > 1) {
+                const tag = createTag(tagText);
+                tagsContainer.insertBefore(tag, tagInput);
+                updateHiddenTags();
+            }
+            e.target.value = '';
+        }
+    });
+
+    tagsContainer.querySelectorAll('.tag button').forEach(button => {
+        button.addEventListener('click', () => {
+            tagsContainer.removeChild(button.parentElement);
+            updateHiddenTags();
+        });
+    });
+
+</script>
 
 
 
