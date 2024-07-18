25ae7a8f128becb4688f433a92ec4b5f2cab5e9d
vikabukach <bukachviko@gmail.com>
2024-07-18 08:36:38 +0300

    added calendar with tasks

--- app/views/index.php
+++ app/views/index.php
@@ -3,7 +3,52 @@
 $title = 'Home page';
 ob_start();
 ?>
+
+
 <h1>Home page</h1>
+<div id='calendar'></div>
+
+<?php $path = '/todo/tasks/task/'; ?>
+
+<script>
+    //get data about tasks from php controller
+    const tasksJson = <?= json_encode($tasksJson); ?>;
+    const tasks = JSON.parse(tasksJson);   // tasks-it is the array of objects
+
+    // Array data conversion in the task for calendar
+    const events = tasks.map((task) => {
+        return {
+            title: task.title,
+            start: new Date(task.created_at),
+            end: new Date(task.finish_date),
+            extendedProps: {
+                task_id: task.id
+            },
+        };
+    });
+
+    //load event handler DOM
+    document.addEventListener("DOMContentLoaded", function() {
+        const calendarEl = document.getElementById('calendar');
+
+        //  initializing the calendar
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            events: events,  //tasks in view events on the calendar
+            eventClick: function(info) {
+            const taskId = info.event.extendedProps.task_id;
+
+            //  url the task
+                const taskUrl = `<?=$path;?>${taskId}`;
+
+                //link page of the task
+                window.location.href = taskUrl;
+        },
+        });
+        calendar.render();
+    });
+</script>
+
 
 <?php $content = ob_get_clean();
 
--- app/views/layout.php
+++ app/views/layout.php
@@ -17,6 +17,8 @@ function is_active($path): string
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/app/css/style.css">
     <script src="https://kit.fontawesome.com/d9195b3006.js" crossorigin="anonymous"></script>
+<       <!--    fullcalendar  https://fullcalendar.io/    -->
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
         <!--    flatpickr-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
