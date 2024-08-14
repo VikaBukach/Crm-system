10d601b95f3311574e7fd75603ea17906785bb77
vikabukach <bukachviko@gmail.com>
2024-08-14 16:05:00 +0300

    fix

--- app/views/users/profile.php
+++ app/views/users/profile.php
@@ -22,6 +22,10 @@ ob_start();
                 </tr>
                 <tr>
                     <th scope="row">Email</th>
+                    <td><?php echo $user['email']; ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">Email verification</th>
                     <td><?php echo $user['email_verification'] ? 'Yes' : 'No'; ?></td>
                 </tr>
                 <tr>
@@ -61,11 +65,11 @@ ob_start();
     <h3 class="mb-4">One-time password generation</h3>
     <h5>Yor otp code: <?=$otp;?></h5>
     <ul class="list-group">
-        <li class="list-group-item">Press<strong>Save the password</strong></li>
+        <li class="list-group-item">Press <strong>Save the password</strong></li>
         <li class="list-group-item">Go to in the Telegram and search the bot: <a target="_blank" href="https://t.me/myDevCRM_bot">@myDevCRM_bot</a>.</li>
-        <li class="list-group-item">Enter the command /email/</li>
-        <li class="list-group-item">The bot will request the OTP code, here is: <strong><?php echo $user['email']; ?></strong> </li>
-        <li class="list-group-item">The bot will request your email, here is: <strong><?php echo $otp; ?></strong> </li>
+        <li class="list-group-item">Enter the command /email</li>
+        <li class="list-group-item">The bot will request your email, here is: <strong><?php echo $user['email']; ?></strong> </li>
+        <li class="list-group-item">The bot will request the OTP code, here is: <strong><?php echo $otp; ?></strong> </li>
         <li class="list-group-item">If you have done this, your accounts will be linked</li>
     </ul>
 
